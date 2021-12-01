a6f1c0f9f3f8ce661d8dc99c8d50587f685b1ec4
Ben Watson <benwatson528@gmail.com>
2021-12-01 09:47:11 +0000

    Removing unneeded square brackets

--- main/day01/sonar_sweep.py
+++ main/day01/sonar_sweep.py
@@ -1,6 +1,6 @@
 def solve_pairs(x) -> int:
-    return sum([1 if b > a else 0 for a, b in zip(x, x[1:])])
+    return sum(1 if b > a else 0 for a, b in zip(x, x[1:]))
 
 
 def solve_sliding(x) -> int:
-    return sum([1 if b + c + d > a + b + c else 0 for a, b, c, d in zip(x, x[1:], x[2:], x[3:])])
+    return sum(1 if b + c + d > a + b + c else 0 for a, b, c, d in zip(x, x[1:], x[2:], x[3:]))
