f38d7d6d7c400e2e2c86d1da84a976677b21f9d2
Ben Watson <benwatson528@gmail.com>
2021-12-03 08:33:08 +0000

    Day 3 part 2

--- main/day03/binary_diagnostic.py
+++ main/day03/binary_diagnostic.py
@@ -2,10 +2,21 @@ import operator
 from collections import Counter
 
 
-def solve(l) -> int:
-    return build_binary(l, operator.gt) * build_binary(l, operator.lt)
+def solve_power_consumption(l) -> int:
+    return int(build_binary(l, operator.gt), 2) * int(build_binary(l, operator.lt), 2)
 
 
-def build_binary(l, relate):
-    return int(
-        ''.join(str(1) if relate(Counter(x)['1'], Counter(x)['0']) else str(0) for x in list(map(list, zip(*l)))), 2)
+def build_binary(l, op):
+    return ''.join(str(1) if op(Counter(x)['1'], Counter(x)['0']) else str(0) for x in list(map(list, zip(*l))))
+
+
+def solve_life_support(l) -> int:
+    oxygen_generator = solve_life_support_rec(l, operator.ge, 0)
+    co2_scrubber = solve_life_support_rec(l, operator.lt, 0)
+    return int(oxygen_generator, 2) * int(co2_scrubber, 2)
+
+
+def solve_life_support_rec(l, op, i):
+    comparison = build_binary(l, op)[i]
+    filtered = list(filter(lambda x: (x[i] == comparison), l))
+    return filtered[0] if len(filtered) == 1 else solve_life_support_rec(filtered, op, i + 1)
--- tests/day03/test_binary_diagnostic.py
+++ tests/day03/test_binary_diagnostic.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day03.binary_diagnostic import solve
+from main.day03.binary_diagnostic import solve_power_consumption, solve_life_support
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 198
+def test_power_consumption_simple():
+    assert solve_power_consumption(read_input("data/test_input.txt")) == 198
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 3882564
+def test_power_consumption_real():
+    assert solve_power_consumption(read_input("data/input.txt")) == 3882564
+
+
+def test_life_support_simple():
+    assert solve_life_support(read_input("data/test_input.txt")) == 230
+
+
+def test_life_support_real():
+    assert solve_life_support(read_input("data/input.txt")) == 3385170
 
 
 def read_input(file_name):
