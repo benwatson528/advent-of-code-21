63798f526fb97f93fb0fb07ca26b0336659f7cbd
Ben Watson <benwatson528@gmail.com>
2021-12-07 08:03:46 +0000

    Day 7 part 2

--- main/day07/the_treachery_of_whales.py
+++ main/day07/the_treachery_of_whales.py
@@ -1,10 +1,19 @@
-def solve(crabs) -> int:
-    min_fuel = 999999
-    for i in range(min(crabs), max(crabs) + 1):
-        fuel_cost = 0
-        for crab in crabs:
-            abs1 = abs(crab - i)
-            fuel_cost += abs1
-        min_fuel = min(min_fuel, fuel_cost)
+import sys
 
+
+def solve_constant_burn(crabs) -> int:
+    min_fuel = sys.maxsize
+    for i in range(min(crabs), max(crabs) + 1):
+        min_fuel = min(min_fuel, sum(abs(crab - i) for crab in crabs))
+    return min_fuel
+
+
+def solve_increasing_burn(crabs) -> int:
+    min_fuel = sys.maxsize
+    for i in range(min(crabs), max(crabs) + 1):
+        total_moved = 0
+        for crab in crabs:
+            distance = abs(crab - i)
+            total_moved += (distance * (distance + 1)) / 2
+        min_fuel = min(min_fuel, total_moved)
     return min_fuel
--- tests/day07/test_the_treachery_of_whales.py
+++ tests/day07/test_the_treachery_of_whales.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day07.the_treachery_of_whales import solve
+from main.day07.the_treachery_of_whales import solve_constant_burn, solve_increasing_burn
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 37
+def test_constant_burn_simple():
+    assert solve_constant_burn(read_input("data/test_input.txt")) == 37
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 3
+def test_constant_burn_real():
+    assert solve_constant_burn(read_input("data/input.txt")) == 349812
+
+
+def test_increasing_burn_simple():
+    assert solve_increasing_burn(read_input("data/test_input.txt")) == 168
+
+
+def test_increasing_burn_real():
+    assert solve_increasing_burn(read_input("data/input.txt")) == 99763899
 
 
 def read_input(file_name):
