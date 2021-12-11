256c3e6772bfb2eb958c990335eddf43eacc048e
Ben Watson <benwatson528@gmail.com>
2021-12-11 13:54:01 +0000

    day 11

--- main/day11/dumbo_octopus.py
+++ main/day11/dumbo_octopus.py
@@ -1,18 +1,13 @@
-def solve(grid) -> int:
+def solve(grid, sync) -> int:
     num_flashes = 0
-    for _ in range(100):
+    for i in range(9999999 if sync else 100):
         flashed = []
         for y in range(len(grid)):
             for x in range(len(grid[y])):
-                if (y, x) in flashed:
-                    continue
-                elif grid[y][x] == 9:
-                    grid[y][x] = 0
-                    flashed.append((y, x))
-                    flashed = bloom(grid, y, x, flashed)
-                else:
-                    grid[y][x] += 1
+                flashed = evolve(grid, y, x, flashed)
         num_flashes += len(flashed)
+        if all(item == 0 for sublist in grid for item in sublist):
+            return i + 1
     return num_flashes
 
 
@@ -28,13 +23,17 @@ def bloom(grid, y, x, flashed):
             (y + 1, x - 1),
             (y - 1, x + 1),
             (y - 1, x - 1)):
-        if (new_y, new_x) in flashed or new_y < 0 or new_x < 0 or new_y >= len(grid) or new_x >= len(grid[new_y]):
-            continue
-        elif grid[new_y][new_x] == 9:
-            grid[new_y][new_x] = 0
-            flashed.append((new_y, new_x))
-            flashed = bloom(grid, new_y, new_x, flashed)
-        else:
-            grid[new_y][new_x] += 1
+        flashed = evolve(grid, new_y, new_x, flashed)
+    return flashed
 
+
+def evolve(grid, y, x, flashed):
+    if (y, x) in flashed or y < 0 or x < 0 or y >= len(grid) or x >= len(grid[y]):
+        return flashed
+    elif grid[y][x] == 9:
+        grid[y][x] = 0
+        flashed.append((y, x))
+        flashed = bloom(grid, y, x, flashed)
+    else:
+        grid[y][x] += 1
     return flashed
--- tests/day11/test_dumbo_octopus.py
+++ tests/day11/test_dumbo_octopus.py
@@ -4,12 +4,20 @@ from pathlib import Path
 from main.day11.dumbo_octopus import solve
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 1656
+def test_turns_simple():
+    assert solve(read_input("data/test_input.txt"), False) == 1656
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 1688
+def test_turns_real():
+    assert solve(read_input("data/input.txt"), False) == 1688
+
+
+def test_sync_simple():
+    assert solve(read_input("data/test_input.txt"), True) == 195
+
+
+def test_sync_real():
+    assert solve(read_input("data/input.txt"), True) == 403
 
 
 def read_input(file_name):
