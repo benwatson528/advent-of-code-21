dab8b0520e746ad462fe6bc2c88be27859ed91df
Ben Watson <benwatson528@gmail.com>
2021-12-12 17:39:13 +0000

    Day 12 part 2

--- main/day12/passage_pathing.py
+++ main/day12/passage_pathing.py
@@ -1,10 +1,16 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 
-def solve(connections) -> int:
+def solve_unlimited(connections) -> int:
     graph = build_graph(connections)
-    grids = find_all_paths(graph, 'start', [])
-    return len(grids)
+    paths = find_all_paths(graph, 'start', [])
+    return len(paths)
+
+
+def solve_limited(connections) -> int:
+    graph = build_graph(connections)
+    paths = find_all_paths_limit(graph, 'start', [])
+    return len(paths)
 
 
 def build_graph(connections):
@@ -24,3 +30,33 @@ def find_all_paths(graph, start, path):
         if (node.islower() and node not in path) or node.isupper():
             paths.extend(find_all_paths(graph, node, path))
     return paths
+
+
+def find_all_paths_limit(graph, start, path):
+    path = path + [start]
+    if start == 'end':
+        return [path]
+    paths = []
+    for node in graph[start]:
+        if is_valid_movement(node, path):
+            paths.extend(find_all_paths_limit(graph, node, path))
+    return paths
+
+
+def is_valid_movement(current, path):
+    counter = Counter(x for x in path if x.islower() and x != current and x != 'start')
+    # We can only visit start once
+    if current == 'start' and len(path) == 1:
+        return True
+    elif current == 'start':
+        return False
+    # We can visit uppers as often as we want
+    elif current.isupper():
+        return True
+    # You can visit any lowercase node once
+    elif current.islower() and path.count(current) == 0:
+        return True
+    # One lower can be visited twice, the others only once
+    elif current.islower() and path.count(current) < 2 and 2 not in counter.values():
+        return True
+    return False
--- tests/day12/test_passage_pathing.py
+++ tests/day12/test_passage_pathing.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day12.passage_pathing import solve
+from main.day12.passage_pathing import solve_limited, solve_unlimited
 
 
-def test_simple():
-    assert solve(read_input("data/test_input.txt")) == 10
+def test_unlimited_simple():
+    assert solve_unlimited(read_input("data/test_input.txt")) == 10
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 5212
+def test_unlimited_real():
+    assert solve_unlimited(read_input("data/input.txt")) == 5212
+
+
+def test_limited_simple():
+    assert solve_limited(read_input("data/test_input.txt")) == 36
+
+
+def test_limited_real():
+    assert solve_limited(read_input("data/input.txt")) == 134862
 
 
 def read_input(file_name):
