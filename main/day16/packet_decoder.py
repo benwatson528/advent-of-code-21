2c27f6010a2b3fae92f1e98cb3268401cd7d8f24
Ben Watson <benwatson528@gmail.com>
2022-10-23 15:30:47 +0100

    Day 16 part 2

--- main/day16/packet_decoder.py
+++ main/day16/packet_decoder.py
@@ -1,20 +1,21 @@
-def solve(h) -> int:
+import math
+
+
+def solve(h) -> (int, int):
     b = (bin(int(h, 16))[2:]).zfill(len(h) * 4)
     filled_b = b.zfill(len(b) + len(b) % 4)
     packet_versions = []
-    parse_packet(filled_b, 0, packet_versions)
-    return sum(packet_versions)
+    _, ans = parse_packet(filled_b, 0, packet_versions)
+    return sum(packet_versions), ans
 
 
 def parse_packet(b, start_idx, packet_versions):
-    start = b[start_idx:]
-    print(f"start = {start}")
     packet_versions.append(int(b[start_idx:start_idx + 3], 2))
     type_id = int(b[start_idx + 3:start_idx + 6], 2)
     if type_id == 4:
         return process_id_4(b, start_idx + 6)
     else:
-        return process_operator(b, start_idx + 6, packet_versions)
+        return process_operator(b, start_idx + 6, packet_versions, type_id)
 
 
 def process_id_4(b, start_idx):
@@ -23,21 +24,42 @@ def process_id_4(b, start_idx):
         groups.extend(b[i + 1:i + 5])
         if b[i] == '0':
             number = int(''.join(groups), 2)
-            print(f"number = {number}")
-            return i + 5
+            return i + 5, number
 
 
-def process_operator(b, start_idx, packet_versions):
+def process_operator(b, start_idx, packet_versions, type_id):
     length_type_id = b[start_idx]
+    expression_args = []
     if length_type_id == '0':
         total_subpackets_length = int(''.join(b[start_idx + 1:start_idx + 15 + 1]), 2)
         idx = start_idx + 1 + 15
         while idx < start_idx + 1 + 15 + total_subpackets_length:
-            idx = parse_packet(b, idx, packet_versions)
-        return idx
+            idx, num = parse_packet(b, idx, packet_versions)
+            expression_args.append(num)
     else:
         num_subpackets = int(''.join(b[start_idx + 1:start_idx + 11 + 1]), 2)
         idx = start_idx + 1 + 11
         for _ in range(num_subpackets):
-            idx = parse_packet(b, idx, packet_versions)
-        return idx
+            idx, num = parse_packet(b, idx, packet_versions)
+            expression_args.append(num)
+
+    return idx, evaluate_expression(expression_args, type_id)
+
+
+def evaluate_expression(expression_args, type_id):
+    if type_id == 0:
+        return sum(expression_args)
+    elif type_id == 1:
+        return math.prod(expression_args)
+    elif type_id == 2:
+        return min(expression_args)
+    elif type_id == 3:
+        return max(expression_args)
+    elif type_id == 5:
+        return 1 if expression_args[0] > expression_args[1] else 0
+    elif type_id == 6:
+        return 1 if expression_args[0] < expression_args[1] else 0
+    elif type_id == 7:
+        return 1 if expression_args[0] == expression_args[1] else 0
+    else:
+        return 0
--- tests/day16/test_packet_decoder.py
+++ tests/day16/test_packet_decoder.py
@@ -4,39 +4,73 @@ from pathlib import Path
 from main.day16.packet_decoder import solve
 
 
-# A single packet with a literal value
 def test_id_4_simple():
-    assert solve("D2FE28") == 6
+    assert solve("D2FE28")[0] == 6
 
 
 def test_2_sub_packets():
-    assert solve("38006F45291200") == 9
+    assert solve("38006F45291200")[0] == 9
 
 
-# 3 sub-packets
 def test_3_sub_packets():
-    assert solve("EE00D40C823060") == 14
+    assert solve("EE00D40C823060")[0] == 14
 
 
 # The set of non-worked examples at the bottom of the page
 def test_examples_1():
-    assert solve("8A004A801A8002F478") == 16
+    assert solve("8A004A801A8002F478")[0] == 16
 
 
 def test_examples_2():
-    assert solve("620080001611562C8802118E34") == 12
+    assert solve("620080001611562C8802118E34")[0] == 12
 
 
 def test_examples_3():
-    assert solve("C0015000016115A2E0802F182340") == 23
+    assert solve("C0015000016115A2E0802F182340")[0] == 23
 
 
 def test_examples_4():
-    assert solve("A0016C880162017C3686B18A3D4780") == 31
+    assert solve("A0016C880162017C3686B18A3D4780")[0] == 31
 
 
-def test_real():
-    assert solve(read_input("data/input.txt")) == 886
+def test_part_1_real():
+    assert solve(read_input("data/input.txt"))[0] == 886
+
+
+def test_operators_sum():
+    assert solve("C200B40A82")[1] == 3
+
+
+def test_operators_product():
+    assert solve("04005AC33890")[1] == 54
+
+
+def test_operators_minimum():
+    assert solve("880086C3E88112")[1] == 7
+
+
+def test_operators_maximum():
+    assert solve("CE00C43D881120")[1] == 9
+
+
+def test_operators_less_than():
+    assert solve("D8005AC2A8F0")[1] == 1
+
+
+def test_operators_greater_than():
+    assert solve("F600BC2D8F")[1] == 0
+
+
+def test_operators_equality():
+    assert solve("9C005AC2F8F0")[1] == 0
+
+
+def test_operators_sum_and_equality():
+    assert solve("9C0141080250320F1802104A08")[1] == 1
+
+
+def test_part_2_real():
+    assert solve(read_input("data/input.txt"))[1] == 184487454837
 
 
 def read_input(file_name):
