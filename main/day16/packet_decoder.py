0e2f36e560202ee6e66acc222f5b77345cf1118e
Ben Watson <benwatson528@gmail.com>
2022-10-23 13:28:11 +0100

    Day 16 part 1

--- main/day16/packet_decoder.py
+++ main/day16/packet_decoder.py
@@ -1,57 +1,43 @@
 def solve(h) -> int:
-    b = convert_to_binary(h)
-    literals = []
-    parse_packet(b, 0, literals)
-    return sum(literals)
+    b = (bin(int(h, 16))[2:]).zfill(len(h) * 4)
+    filled_b = b.zfill(len(b) + len(b) % 4)
+    packet_versions = []
+    parse_packet(filled_b, 0, packet_versions)
+    return sum(packet_versions)
 
 
-def parse_packet(b, start_idx, literals):
-    packet_version = int(b[start_idx:start_idx + 3], 2)
+def parse_packet(b, start_idx, packet_versions):
+    start = b[start_idx:]
+    print(f"start = {start}")
+    packet_versions.append(int(b[start_idx:start_idx + 3], 2))
     type_id = int(b[start_idx + 3:start_idx + 6], 2)
     if type_id == 4:
-        return process_id_4(b, start_idx + 6, literals)
+        return process_id_4(b, start_idx + 6)
     else:
-        return process_operator(b, start_idx + 6, literals)
+        return process_operator(b, start_idx + 6, packet_versions)
 
 
-def process_id_4(b, start_idx, literals):
+def process_id_4(b, start_idx):
     groups = []
     for i in range(start_idx, len(b), 5):
         groups.extend(b[i + 1:i + 5])
         if b[i] == '0':
-            literals.append(int(''.join(groups), 2))
-            return i + 5 + 3  # 3 0's after
+            number = int(''.join(groups), 2)
+            print(f"number = {number}")
+            return i + 5
 
 
-def process_operator(b, start_idx, literals):
+def process_operator(b, start_idx, packet_versions):
     length_type_id = b[start_idx]
     if length_type_id == '0':
         total_subpackets_length = int(''.join(b[start_idx + 1:start_idx + 15 + 1]), 2)
-        idx = start_idx + 1 + 15 # This is where we start
+        idx = start_idx + 1 + 15
         while idx < start_idx + 1 + 15 + total_subpackets_length:
-            idx = parse_packet(b, idx, literals)
+            idx = parse_packet(b, idx, packet_versions)
         return idx
     else:
         num_subpackets = int(''.join(b[start_idx + 1:start_idx + 11 + 1]), 2)
+        idx = start_idx + 1 + 11
         for _ in range(num_subpackets):
-            idx = parse_packet(b, start_idx + 1 + idx, literals)
-    return idx
-
-
-def convert_to_binary(h):
-    return h.replace('0', '0000') \
-        .replace('1', '0001') \
-        .replace('2', '0010') \
-        .replace('3', '0011') \
-        .replace('4', '0100') \
-        .replace('5', '0101') \
-        .replace('6', '0110') \
-        .replace('7', '0111') \
-        .replace('8', '1000') \
-        .replace('9', '1001') \
-        .replace('A', '1010') \
-        .replace('B', '1011') \
-        .replace('C', '1100') \
-        .replace('D', '1101') \
-        .replace('E', '1110') \
-        .replace('F', '1111')
+            idx = parse_packet(b, idx, packet_versions)
+        return idx
--- tests/day16/test_packet_decoder.py
+++ tests/day16/test_packet_decoder.py
@@ -4,18 +4,39 @@ from pathlib import Path
 from main.day16.packet_decoder import solve
 
 
-# a single packet with a number
+# A single packet with a literal value
 def test_id_4_simple():
-    assert solve("D2FE28") == 2021
+    assert solve("D2FE28") == 6
 
 
-# # 2 sub-packets
-# def test_length_id_0_simple():
-#     assert solve("38006F45291200") == 30
-#
-#
-# def test_real():
-#     assert solve(read_input("data/input.txt")) == 0
+def test_2_sub_packets():
+    assert solve("38006F45291200") == 9
+
+
+# 3 sub-packets
+def test_3_sub_packets():
+    assert solve("EE00D40C823060") == 14
+
+
+# The set of non-worked examples at the bottom of the page
+def test_examples_1():
+    assert solve("8A004A801A8002F478") == 16
+
+
+def test_examples_2():
+    assert solve("620080001611562C8802118E34") == 12
+
+
+def test_examples_3():
+    assert solve("C0015000016115A2E0802F182340") == 23
+
+
+def test_examples_4():
+    assert solve("A0016C880162017C3686B18A3D4780") == 31
+
+
+def test_real():
+    assert solve(read_input("data/input.txt")) == 886
 
 
 def read_input(file_name):
