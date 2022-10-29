0c9c9b7a386b304be85864d3110394c911fabbc6
Ben Watson <benwatson528@gmail.com>
2022-10-29 07:41:50 +0100

    Day 17 part 2 tests

--- tests/day17/test_trick_shot.py
+++ tests/day17/test_trick_shot.py
@@ -5,11 +5,15 @@ from main.day17.trick_shot import solve
 
 
 def test_simple():
-    assert solve(parse_input("target area: x=20..30, y=-10..-5")) == 45
+    max_y, num_successful_velocities = solve(parse_input("target area: x=20..30, y=-10..-5"))
+    assert max_y == 45
+    assert num_successful_velocities == 112
 
 
 def test_real():
-    assert solve(parse_input("target area: x=128..160, y=-142..-88")) == 10011
+    max_y, num_successful_velocities = solve(parse_input("target area: x=128..160, y=-142..-88"))
+    assert max_y == 10011
+    assert num_successful_velocities == 2994
 
 
 def parse_input(x):
